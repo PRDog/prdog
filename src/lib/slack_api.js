887bfb89094090303f9ce0334e808889b78c7612
Lorenzo Fundaro <lorenzo.fundaro@goeuro.com>
2018-03-05 23:55:36 +0100

    unescape text, log slack api errors

--- src/lib/slack_api.js
+++ src/lib/slack_api.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const WebClient = require('@slack/client').WebClient;
-const logger = require('./logger')
+const logger = require('./logger');
 
 let client;
 
@@ -11,18 +11,18 @@ try {
     process.exit(1);
 }
 
-const sendSlackMessage = (to, slackMsg) => {
+const sendSlackMessage = async (to, slackMsg) => {
     logger.info("Sending slack message to " + to);
     try {
-        client.chat.postMessage(`@${to}`, null, {'attachments': slackMsg});
+        await client.chat.postMessage(`@${to}`, null, {'attachments': slackMsg});
     } catch (err) {
         logger.error(`Could not send slack message to ${to}: ${err}`);
     }
 };
 
-const updateMessage = (payload, slackMsg) => {
+const updateMessage = async (payload, slackMsg) => {
     try {
-        client.chat.update(payload.original_message.ts, payload.channel.id, null, {'attachments': slackMsg});
+        await client.chat.update(payload.original_message.ts, payload.channel.id, null, {'attachments': slackMsg});
     } catch (e) {
         logger.error(`Could not update slack message: ${e}`);
     }
--- src/pull_request_review_handler.js
+++ src/pull_request_review_handler.js
@@ -3,7 +3,6 @@ const { sendSlackMessage } = require('./lib/slack_api.js');
 const { formatUser } = require('./lib/pr_utils.js');
 const Mustache = require('mustache');
 const templates = require('./lib/templates.js').loadTemplates();
-const logger = require('./lib/logger');
 
 const notifySubmitted = (event) => {
     const slackMsg = Mustache.render(templates.get(['pr', event.action, event.review.state].join('_')), {
@@ -13,18 +12,22 @@ const notifySubmitted = (event) => {
         repo: event.repository,
     });
     sendSlackMessage(`${userMap.get(event.pull_request.user.login)}`, slackMsg);
-}
+};
 
 const pullRequestReviewHandler = (req, res) => {
     res.status(200).end();
     const event = req.body;
     const action = event.action;
+    const state = event.review.state;
     switch (action) {
         case 'submitted':
-            notifySubmitted(event);
+            // We don't support for now other states like commented
+            if (state === 'approved' || state === 'changes_requested') {
+                notifySubmitted(event);
+            }
             break;
         default:
-            logger.info(`action not supported: ${action}`);
+            break;
 
     }
 };
